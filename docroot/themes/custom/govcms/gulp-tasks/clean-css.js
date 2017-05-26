36829731655af527ba08e9020e1f247ed39e713d
Steven Worley <sj.worley88@gmail.com>
2017-05-26 14:41:14 +1000

    GOVCMS-000: Adds custom themes to the repository.
    - Adds custom theme from cog staterkit.
    - Adds custom theme using govcms-ui as a base.
    - Adds govcms-ui to composer.
    - Updates composer to include a patch for core.

---
+++ docroot/themes/custom/govcms/gulp-tasks/clean-css.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Task: Clean:CSS.
+ */
+
+module.exports = function (gulp, plugins, options) {
+  'use strict';
+
+  // Clean CSS files.
+  gulp.task('clean:css', function () {
+    plugins.del.sync([
+      options.css.files
+    ]);
+  });
+};
