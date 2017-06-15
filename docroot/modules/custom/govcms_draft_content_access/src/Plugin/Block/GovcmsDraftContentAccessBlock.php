36b2bf87045336b3781b8e441f81a748a2fa498c
Yusuf Hasan Miyan <yusuf.hasanmiyan@gmail.com>
2017-06-16 07:07:54 +1000

    GDE-012: Replaced nested ifs with early returns.

--- docroot/modules/custom/govcms_draft_content_access/src/Plugin/Block/GovcmsDraftContentAccessBlock.php
+++ docroot/modules/custom/govcms_draft_content_access/src/Plugin/Block/GovcmsDraftContentAccessBlock.php
@@ -114,141 +114,148 @@ class GovcmsDraftContentAccessBlock extends BlockBase implements ContainerFactor
 
     // Get node and it's moderation state.
     $node = $this->currentRouteMatch->getParameter('node');
-    if ($node) {
-      $moderation_value = $node->get('moderation_state')->getValue();
-      $moderation_state = reset($moderation_value);
-      if (isset($moderation_state['target_id']) && $moderation_state['target_id'] == 'draft') {
-        $system_roles = user_role_names(TRUE);
-        // Make sure role already created, no check on module permission.
-        if (array_key_exists('draft_content_access', $system_roles)) {
-          $autologin_config = $this->configFactory->get('auto_login_url.settings');
-          $autologin_config_expired = $autologin_config->get('expiration');
+    if (!$node) {
+      return $build;
+    }
 
-          $user = $this->userStorage->loadByProperties(['name' => 'autologin']);
+    $moderation_value = $node->get('moderation_state')->getValue();
+    $moderation_state = reset($moderation_value);
+    if (!isset($moderation_state['target_id']) || $moderation_state['target_id'] != 'draft') {
+      return $build;
+    }
 
-          if (!empty($user)) {
-            $uid_data = array_keys($user);
-            $uid = reset($uid_data);
-            $destination = $this->currentPath->getPath();
+    $system_roles = user_role_names(TRUE);
+    // Make sure role already created, no check on module permission.
+    if (!array_key_exists('draft_content_access', $system_roles)) {
+      return $build;
+    }
 
-            // Check existing data or create new autologin link.
-            $query = $this->connection->select('auto_login_url_govcms', 'a');
-            $query->fields('a');
-            $query->condition('uid', $uid, '=');
-            $query->condition('destination', ltrim($destination, "/"), '=');
-            $query->orderBy('timestamp', 'DESC');
+    $autologin_config = $this->configFactory->get('auto_login_url.settings');
+    $autologin_config_expired = $autologin_config->get('expiration');
 
-            $result = $query->execute()->fetchAll();
-            if ($result) {
+    $user = $this->userStorage->loadByProperties(['name' => 'autologin']);
+    if (empty($user)) {
+      return $build;
+    }
 
-              // Table header.
-              $header = [
-                'link' => $this->t('Copy to clipboard'),
-                'plain_link' => $this->t('Manual link copy'),
-                'expired' => $this->t('Link expires'),
-                'operation' => $this->t('Operation'),
-              ];
+    $uid_data = array_keys($user);
+    $uid = reset($uid_data);
+    $destination = $this->currentPath->getPath();
 
-              $rows = [];
+    // Check existing data or create new autologin link.
+    $query = $this->connection->select('auto_login_url_govcms', 'a');
+    $query->fields('a');
+    $query->condition('uid', $uid, '=');
+    $query->condition('destination', ltrim($destination, "/"), '=');
+    $query->orderBy('timestamp', 'DESC');
 
-              foreach ($result as $row) {
-                $date_interval = ($row->timestamp + $autologin_config_expired) - time();
-                $date_format_interval = ($date_interval < 0) ? $this->t('Expired') : $this->dateFormatter->formatInterval($date_interval);
+    $result = $query->execute()->fetchAll();
+    if ($result) {
 
-                $autologin_link = Url::fromRoute('auto_login_url.login', [
-                  'uid' => $uid,
-                  'hash' => $row->hash,
-                ], [
-                  'absolute' => TRUE,
-                ]);
+      // Table header.
+      $header = [
+        'link' => $this->t('Copy to clipboard'),
+        'plain_link' => $this->t('Manual link copy'),
+        'expired' => $this->t('Link expires'),
+        'operation' => $this->t('Operation'),
+      ];
 
-                $revoke_url = Url::fromRoute('govcms_draft_content_access.revokelink', [
-                  'hash' => $row->hash,
-                  'destination' => str_replace("/", "-", $destination),
-                ], [
-                  'attributes' => [
-                    'class' => [
-                      'btn',
-                      'btn-danger',
-                    ],
-                  ],
-                ]);
+      $rows = [];
 
-                $link_plain = $autologin_link;
+      foreach ($result as $row) {
+        $date_interval = ($row->timestamp + $autologin_config_expired) - time();
+        $date_format_interval = ($date_interval < 0) ? $this->t('Expired') : $this->dateFormatter->formatInterval($date_interval);
 
-                $link_html = Url::fromUri('internal:#', [
-                  'attributes' => [
-                    'class' => [
-                      'btn',
-                      'btn-primary',
-                      'btn-clipboard',
-                    ],
-                    'data-clipboard-text' => [
-                      $autologin_link->toString(),
-                    ],
-                  ],
-                ]);
+        $autologin_link = Url::fromRoute('auto_login_url.login', [
+          'uid' => $uid,
+          'hash' => $row->hash,
+        ], [
+          'absolute' => TRUE,
+        ]);
 
-                $rows[$row->id] = [
-                  'link' => \Drupal::l($this->t('Copy'), $link_html),
-                  'plain_link' => \Drupal::l($this->t('Right click and copy link'), $link_plain),
-                  'expired' => $date_format_interval,
-                  'operation' => \Drupal::l($this->t('Revoke link'), $revoke_url),
-                ];
-              }
+        $revoke_url = Url::fromRoute('govcms_draft_content_access.revokelink', [
+          'hash' => $row->hash,
+          'destination' => str_replace("/", "-", $destination),
+        ], [
+          'attributes' => [
+            'class' => [
+              'btn',
+              'btn-danger',
+            ],
+          ],
+        ]);
 
-              $generate_url = Url::fromRoute('govcms_draft_content_access.generatelink', [
-                'uid' => $uid,
-                'destination' => str_replace("/", "-", $destination),
-              ], [
-                'attributes' => [
-                  'class' => [
-                    'btn',
-                    'btn-success',
-                  ],
-                ],
-              ]);
+        $link_plain = $autologin_link;
 
-              $build = [
-                '#prefix' => $this->t('<p>List of existing access link to this content. Click revoke to remove existing/expired link.</p>'),
-                '#type' => 'table',
-                '#header' => $header,
-                '#rows' => $rows,
-                '#empty' => $this->t('No autologin link found for this node.'),
-                '#suffix' => \Drupal::l($this->t('Generate link'), $generate_url),
-                '#attached' => [
-                  'library' => ['govcms_draft_content_access/ea-dca'],
-                ],
-                '#cache' => [
-                  'max-age' => 0,
-                ],
-              ];
-            }
-            else {
-              $generate_url = Url::fromRoute('govcms_draft_content_access.generatelink', [
-                'uid' => $uid,
-                'destination' => str_replace("/", "-", $destination),
-              ], [
-                'attributes' => [
-                  'class' => [
-                    'btn',
-                    'btn-success',
-                  ],
-                ],
-              ]);
+        $link_html = Url::fromUri('internal:#', [
+          'attributes' => [
+            'class' => [
+              'btn',
+              'btn-primary',
+              'btn-clipboard',
+            ],
+            'data-clipboard-text' => [
+              $autologin_link->toString(),
+            ],
+          ],
+        ]);
 
-              $build = [
-                '#prefix' => $this->t('<p>Click generate button to create access link to this content.</p>'),
-                '#type' => 'markup',
-                '#markup' => \Drupal::l($this->t('Generate link'), $generate_url),
-                '#cache' => [
-                  'max-age' => 0,
-                ],
-              ];
-            }
-          }
-        }
+        $rows[$row->id] = [
+          'link' => \Drupal::l($this->t('Copy'), $link_html),
+          'plain_link' => \Drupal::l($this->t('Right click and copy link'), $link_plain),
+          'expired' => $date_format_interval,
+          'operation' => \Drupal::l($this->t('Revoke link'), $revoke_url),
+        ];
       }
+
+      $generate_url = Url::fromRoute('govcms_draft_content_access.generatelink', [
+        'uid' => $uid,
+        'destination' => str_replace("/", "-", $destination),
+      ], [
+        'attributes' => [
+          'class' => [
+            'btn',
+            'btn-success',
+          ],
+        ],
+      ]);
+
+      $build = [
+        '#prefix' => $this->t('<p>List of existing access link to this content. Click revoke to remove existing/expired link.</p>'),
+        '#type' => 'table',
+        '#header' => $header,
+        '#rows' => $rows,
+        '#empty' => $this->t('No autologin link found for this node.'),
+        '#suffix' => \Drupal::l($this->t('Generate link'), $generate_url),
+        '#attached' => [
+          'library' => ['govcms_draft_content_access/ea-dca'],
+        ],
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ];
+    }
+    else {
+      $generate_url = Url::fromRoute('govcms_draft_content_access.generatelink', [
+        'uid' => $uid,
+        'destination' => str_replace("/", "-", $destination),
+      ], [
+        'attributes' => [
+          'class' => [
+            'btn',
+            'btn-success',
+          ],
+        ],
+      ]);
+
+      $build = [
+        '#prefix' => $this->t('<p>Click generate button to create access link to this content.</p>'),
+        '#type' => 'markup',
+        '#markup' => \Drupal::l($this->t('Generate link'), $generate_url),
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ];
     }
 
     return $build;
